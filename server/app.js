8e4de28ecd5de9299752134c07c589217ca2d55f
Manzi Fabrice <manzif60@gmail.com>
2019-09-26 11:59:32 +0200

    ch(heroku): fixing heroku buy adding nodemon in the packagei

--- server/app.js
+++ server/app.js
@@ -3,19 +3,11 @@ import express from 'express'
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import routes from './routes';
+import cors from 'cors';
+import '@babel/polyfill';
 
-const hostname = '127.0.0.1';
-const port = 5000;
-const app = express()
-const server = http.createServer(app);
-
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS, DELETE, PATCH');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
-
+const app = express();
+app.use(cors());
 app.use(logger('dev'));
 app.use('/uploads', express.static('server/uploads'))
 app.use(bodyParser.json({limit: "50mb"}));
@@ -23,9 +15,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 routes(app);
 
 app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the .',
+  message: 'Welcome to the E-service',
 }));
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const port = process.env.PORT || 5000;
+app.listen(port, function(){
+    console.log(`listening on port ${port}...`);
 });
